c96d4278e874b9e5dfcc95eee33b355c996903e9
0miik0 <danielajarosova182@seznam.cz>
2024-05-10 10:48:04 +0200

    config done

--- app/Config/PaginationConfig.php
+++ app/Config/PaginationConfig.php
@@ -2,11 +2,8 @@
 namespace Config;
 use CodeIgniter\Config\BaseConfig;
 
-class MyConfig extends BaseConfig 
+class PaginationConfig extends BaseConfig 
 {
     public $pagination = "4";
-    //need to add this stuff somewhere in controller then use it, no time to figure that out now: 
-    //$config = new MyConfig();
-    //$promenna = $config->promenna;
 }
 ?>
--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -5,17 +5,20 @@ use App\Controllers\BaseController;
 use App\Models\Typkomponent;
 use App\Models\Komponent;
 use App\Models\Vyrobce;
+use Config\PaginationConfig;
 
 class Main extends BaseController
 {
     var $typKomponent;
     var $komponent;
     var $vyrobce;
+    var $config;
 
     public function __construct(){
         $this->typKomponent = new typkomponent();
         $this->komponent = new komponent();
         $this->vyrobce = new vyrobce();
+        $this->config = new PaginationConfig();
     }
 
     public function getTypyKomponent(){
@@ -24,7 +27,9 @@ class Main extends BaseController
         echo view('TypyKomponent', $data);
     }
     public function getKomponenty($idtypykomponent){
-        $data['komponenty'] = $this->komponent->where('typKomponent_id',$idtypykomponent)->paginate(4);
+       $pagination = $this->config->pagination;
+
+        $data['komponenty'] = $this->komponent->where('typKomponent_id',$idtypykomponent)->paginate($pagination);
         $data ['pager'] = $this->komponent->pager;
         //var_dump($data['komponenty']);
         echo view('VypisKomponent', $data);
